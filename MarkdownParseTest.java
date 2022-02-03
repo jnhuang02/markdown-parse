6b4f9e14a8310d1e6441f2bd61f8212bc09d9764
Justin Huang <justinstarwar@gmail.com>
2022-02-02 20:44:44 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -2,6 +2,7 @@
    
 import static org.junit.Assert.*;
 
+import java.beans.Transient;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,10 +19,19 @@ public class MarkdownParseTest {
 
     @Test
     public void getLinksTester() throws IOException {
-        String contentsTest = Files.readString(Path.of("test-file.md"));
+        String contentsTest = Files.readString(Path.of("/Users/justinhuang/Documents/GitHub/markdown-parse/test-file.md"));
+        assertEquals(List.of("https://something.com", "some-page.html"),
+            MarkdownParse.getLinks(contentsTest));
+    }
+
+    @Test
+    public void failTest() throws IOException{
+        String contentsTest = Files.readString(Path.of("/Users/justinhuang/Documents/GitHub/markdown-parse/test-file.md"));
         assertEquals(List.of("fail test", "some-page.html"),
             MarkdownParse.getLinks(contentsTest));
+
+    }
         
         
-   }
+   
 }
